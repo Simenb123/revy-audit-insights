36f838fa18bae05a345df39ce598c0d6cbf6c6ce
Simenb123 <simenb89@gmail.com>
2025-07-07 16:31:38 +0200

    style: update sidebar bg and border

---
+++ src/components/Layout/SidebarContent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SidebarContent as ShadcnSidebarContent } from '@/components/ui/sidebar'
+import SidebarNav from './SidebarNav'
+
+const SidebarContent = () => (
+  <ShadcnSidebarContent>
+    <SidebarNav />
+  </ShadcnSidebarContent>
+)
+
+export default SidebarContent
