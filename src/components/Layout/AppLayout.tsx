a212e92d66c542aa2ff4bcfe9649219d210a7c4c
Simenb123 <simenb89@gmail.com>
2025-07-07 09:18:02 +0200

    Refactor AppLayout to remove duplicate return

--- src/components/Layout/AppLayout.tsx
+++ src/components/Layout/AppLayout.tsx
@@ -5,11 +5,10 @@ import { Outlet, Navigate } from 'react-router-dom';
 import { useAuth } from '@/components/Auth/AuthProvider';
 import { useUserProfile } from '@/hooks/useUserProfile';
 import AppHeader from './AppHeader';
-import ContextualSidebar from './ContextualSidebar';
 import ResizableRightSidebar from './ResizableRightSidebar';
 import PageLoader from './PageLoader';
 import OnboardingCheck from './OnboardingCheck';
-import { SidebarProvider } from '@/components/ui/sidebar';
+import { SidebarProvider, SidebarContent } from '@/components/ui/sidebar';
 import {
   ResizablePanelGroup,
   ResizablePanel,
@@ -38,43 +37,9 @@ const AppLayout = () => {
 
   return (
     <SidebarProvider>
-      <div className="flex flex-col min-h-screen w-full bg-background">
-        <AppHeader />
+      <AppHeader />
 
-        <div className="flex-1 overflow-y-auto">
-          <ResizablePanelGroup
-            direction="horizontal"
-            className="flex w-full min-h-screen"
-          >
-            <ResizablePanel
-              defaultSize={20}
-              minSize={20}
-              maxSize={20}
-              collapsible
-              collapsedSize={3.5}
-            >
-              <ContextualSidebar />
-            </ResizablePanel>
-            <ResizablePanel>
-              <div className="flex w-full h-full">
-                <main className="flex-1 h-full overflow-auto">
-                  <Outlet />
-                </main>
-                <ResizableRightSidebar />
-              </div>
-            </ResizablePanel>
-          </ResizablePanelGroup>
-        </div>
-      </div>
-    </SidebarProvider>
-  );
-}
-
-return (
-  <SidebarProvider>
-    <AppHeader />
-
-    <div className="flex-1 min-h-screen bg-background">
+      <div className="flex-1 min-h-screen bg-background">
       <ResizablePanelGroup direction="horizontal" className="flex w-full min-h-screen">
 
         {/* VENSTRE – ikon-/tekst-sidebar */}
@@ -100,7 +65,8 @@ return (
         {/* <ResizableRightSidebar /> */}
       </ResizablePanelGroup>
     </div>
-  </SidebarProvider>
-);
+    </SidebarProvider>
+  );
+};
 
 export default AppLayout;
