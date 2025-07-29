6e4c435bdeb744f3d73584c28263cca644b2cea4
Simenb123 <simenb89@gmail.com>
2025-07-29 06:08:30 -0700

    Refactor admin page layout

--- src/pages/AIRevyAdmin.tsx
+++ src/pages/AIRevyAdmin.tsx
@@ -1,20 +1,13 @@
 
 import React from 'react';
 import EnhancedAdminDashboard from '@/components/AIRevyAdmin/EnhancedAdminDashboard';
-import ResponsiveLayout from '@/components/Layout/ResponsiveLayout';
-import ConstrainedWidth from '@/components/Layout/ConstrainedWidth';
-import StandardPageLayout from '@/components/Layout/StandardPageLayout';
-import PageHeader from '@/components/Layout/PageHeader';
+import PageLayout from '@/components/Layout/PageLayout';
 
 const AIRevyAdmin = () => {
   return (
-    <ResponsiveLayout>
-      <ConstrainedWidth width="full">
-        <StandardPageLayout header={<PageHeader title="AI Revy Admin" />}>
-          <EnhancedAdminDashboard />
-        </StandardPageLayout>
-      </ConstrainedWidth>
-    </ResponsiveLayout>
+    <PageLayout width="full">
+      <EnhancedAdminDashboard />
+    </PageLayout>
   );
 };
 
