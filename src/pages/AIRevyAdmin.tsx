5e19e8d66781e463662d94a8f30da97d8501c10a
Simenb123 <simenb89@gmail.com>
2025-07-29 06:09:35 -0700

    Refactor admin page layout

--- src/pages/AIRevyAdmin.tsx
+++ src/pages/AIRevyAdmin.tsx
@@ -1,9 +1,14 @@
 
 import React from 'react';
 import EnhancedAdminDashboard from '@/components/AIRevyAdmin/EnhancedAdminDashboard';
+import PageLayout from '@/components/Layout/PageLayout';
 
 const AIRevyAdmin = () => {
-  return <EnhancedAdminDashboard />;
+  return (
+    <PageLayout width="full">
+      <EnhancedAdminDashboard />
+    </PageLayout>
+  );
 };
 
 export default AIRevyAdmin;
